d24a06b0107812fa9ac41679fc5a1fc7fea4fe59
천수민 <102133017+smthousand@users.noreply.github.com>
2022-10-18 17:24:41 +0900

    [천] landing page 퍼블리싱 완료
    firebase를 이용해 하단 제출 submit 버튼을 통해 이메일 주소 데이터를 수집하는 작업이 필요합니다

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        btYellow: 'hsl(57, 97%, 71%)',
+        btBk20: 'hsl(240, 20%, 98%)',
+        btBk40: 'hsl(240, 9%, 89%)',
+        btBk60: 'hsl(240, 3%, 74%)',
+        btBk80: 'hsl(240, 1%, 50%)',
+        btBk: 'hsl(240, 2%, 25%)',
+      },
+      fontFamily: {
+        sans: ['SUIT Variable', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
